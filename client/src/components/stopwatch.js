3198bc5d801db3da66aabf9ec24c99f052ed9b29
David Scherer-ODell <dscherer21@gmail.com>
2020-02-06 00:04:35 -0600

    added somee css styling

--- client/src/components/stopwatch.js
+++ client/src/components/stopwatch.js
@@ -1,17 +1,21 @@
 import React from "react";
 import './stopwatch.css';
-import { Jumbotron } from "reactstrap";
+import { Jumbotron, Button } from "reactstrap";
 
 const Stopwatch = () => {
     return(
       <header className='container'>
         <div className='row'>
             <div className='col-md-6 col-xs-12'>
-                <Jumbotron>Fullmetal Alchemist Stopwatch</Jumbotron>
-                <Jumbotron>00:00:00</Jumbotron>
-                <button>Start</button>
-                <button>Stop</button>
-                <button>Reset</button>
+                <Jumbotron>
+                    <div className='fontgradient'>Fullmetal Alchemist Stopwatch</div>
+                </Jumbotron>
+                <Jumbotron>
+                    <div className='fontgradient'>00:00:00</div>
+                </Jumbotron>
+                <button className='btn btn-primary'>Start</button>
+                <button className='btn btn-danger'>Stop</button>
+                <button className='btn btn-warning'>Reset</button>
             
             </div>
         </div>
