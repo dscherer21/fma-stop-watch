755d874e32d3ca726023489145c81263e094333b
David Scherer-ODell <dscherer21@gmail.com>
2020-02-11 00:42:35 -0600

    converted to component class, added constructor and state, spaced out buttons

--- client/src/components/stopwatch.js
+++ client/src/components/stopwatch.js
@@ -2,25 +2,35 @@ import React from "react";
 import './stopwatch.css';
 import { Jumbotron } from "reactstrap";
 
-const Stopwatch = () => {
-    return(
-      <header className='container'>
-        <div className='row justify-content-md-center'>
-            <div className='col-md-6 col-xs-12'>
-                <Jumbotron>
-                    <div className='fontgradient'>Fullmetal Alchemist Stopwatch</div>
-                </Jumbotron>
-                <Jumbotron>
-                    <div className='fontgradient'>00:00:00</div>
-                </Jumbotron>
-                <button className='btn btn-primary'>Start</button>
-                <button className='btn btn-danger'>Stop</button>
-                <button className='btn btn-warning'>Reset</button>
-            
-            </div>
-        </div>
-      </header>
-    )
+class Stopwatch extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            minutes: 0,
+            seconds: 0,
+            miliseconds: 0
+        }
+    }
+
+    render() {
+        return(
+            <header className='container'>
+                <div className='row justify-content-md-center'>
+                    <div className='col-md-6 col-xs-12'>
+                        <Jumbotron>
+                            <div className='fontgradient'>Fullmetal Alchemist Stopwatch</div>
+                        </Jumbotron>
+                        <Jumbotron>
+                            <div className='fontgradient'>{this.state.minutes}:{this.state.seconds}:{this.state.miliseconds}</div>
+                        </Jumbotron>
+                        <button className='btn btn-primary'>Start</button>
+                        <button className='btn btn-danger'>Stop</button>
+                        <button className='btn btn-warning'>Reset</button>
+                    </div>
+                </div>
+            </header>
+        )
+    }
 }
 
 
