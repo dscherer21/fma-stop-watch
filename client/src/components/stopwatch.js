a2fd56bd5fdaa3d6db4d2bed077011c882e954aa
David Scherer-ODell <dscherer21@gmail.com>
2020-03-10 15:41:19 -0500

    fixed issue with jumbotron not working. creating setInterval and clearInterval variables.

--- client/src/components/stopwatch.js
+++ client/src/components/stopwatch.js
@@ -5,50 +5,54 @@ import { Jumbotron } from "reactstrap";
 function Stopwatch() {
 
     /*utilizing useState to set the state with React Hooks*/
-    const [mSecs, setMSecs] = useState(0);
     const [secs, setSecs] = useState(0);
     const [mins, setMins] = useState(0);
+    const [hours, setHours] = useState(0);
+    const [tick, setTick] = useState();
 
     /*function to start the timer*/
     function startTimer() {
         console.log("Start Timer Button clicked!");
-        //setInterval(() => (setMSecs(mSecs + 1), console.log(mSecs)), 1000);
-        setMSecs(mSecs + 1);
-        console.log(mSecs);
+        /*setting tick as state so that it retains the setInterval function across re-renders*/
+        setTick(setInterval(() => setSecs(secs => secs + 1), 1000));
     }
 
     /*function to stop the timer*/
     const stopTimer = () => {
         console.log('Stop Button clicked!');
-        clearInterval();
+        clearInterval(tick);
     }
 
     /*function to reset the timer*/
     function resetTimer(e){
         console.log('Reset Button clicked!');
-        setMSecs(mSecs + 1);
-        console.log(mSecs);
+        clearInterval(tick);
+        setSecs(0);
+        setMins(0);
+        setHours(0);
     }
 
     return(
         <header className='container'>
             <div className='row justify-content-md-center'>
                 <div className='col-md-6 col-xs-12'>
+
                     <Jumbotron>
                         <div className='fontgradient'>Fullmetal Alchemist Stopwatch</div>
                     </Jumbotron>
+
                     <div className='jumbotron'>
-                        {/*This is the original state call. For some reason, it will not render the state change here.*/}
                         <p className='fontgradient'>
-                            {mSecs}
+                            {hours}:{mins}:{secs}
                         </p>
                     </div>
-                    {/*Here's where I am calling the state outside the jumbotron tag. It works here.*/}
-                    <p>{mSecs}</p>
+
                     {/*when the Start button is clicked, it will run the startTimer function*/}
                     <button className='btn btn-primary' onClick={startTimer}>Start</button>
+
                     {/*when the Stop button is clicked, it will run the stopTimer function*/}
                     <button className='btn btn-danger' onClick={stopTimer}>Stop</button>
+
                     {/*when the Reset button is clicked, it will run the resetTimer function*/}
                     <button className='btn btn-warning' onClick={resetTimer}>Reset</button>
                 </div>
